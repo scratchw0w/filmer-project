533257a40ee9b75c8bbb98533368ee306c34ebb4
Max Basov <rikle_ng@ukr.net>
2020-08-27 15:54:01 +0300

    Update filmPageScript.js

--- src/main/resources/static/scripts/filmPageScript.js
+++ src/main/resources/static/scripts/filmPageScript.js
@@ -1,6 +1,11 @@
+
+
 async function getFilms() {
+    const title = "Titanic";
+    const year = 1997;
+
     const films = await axios.get(
-        "http://www.omdbapi.com/?i=tt3896198&apikey=a3d4f820"
+        `http://www.omdbapi.com/?t=${title}&y=${year}&apikey=a3d4f820`
     );
     const film = films.data;
     console.log(film);
