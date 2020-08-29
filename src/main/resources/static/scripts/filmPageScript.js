b44cff4e6d50750b3dcc1b8fa0dc897847c62981
Denys Matsenko <idanchik47@gmail.com>
2020-08-29 12:39:32 +0300

    changed styles when hovering over the film item

--- src/main/resources/static/scripts/filmPageScript.js
+++ src/main/resources/static/scripts/filmPageScript.js
@@ -12,7 +12,7 @@ async function getFilms() {
     console.log(film);
 }
 
-async function getPoster(){
+async function getPoster() {
     for (const item of listItems) {
         const title = item.querySelector("h5").textContent;
         const filmData = await axios.get(
@@ -22,20 +22,68 @@ async function getPoster(){
         const plot = filmData.data.Plot;
         const imgTag = item.querySelector("img");
         item.setAttribute("data-plot", plot);
-        item.querySelector("#plot").textContent = plot
+        item.querySelector("#plot").textContent = plot;
         console.log(imgTag);
         imgTag.src = posterUrl;
     }
 }
 
+function setHoverForFilmItem() {
+    for (const item of listItems) {
+        item.addEventListener("mouseenter", (event) => {
+            console.log(event.target.tagName === "LI");
+            console.log("entering...");
+            if (item.classList.contains("liked")) {
+                item.classList.add("brighter-green");
+            } else {
+                item.classList.add("brighter-white");
+            }
+        });
+
+        item.addEventListener("mouseleave", (event) => {
+            console.log(event.target.tagName === "LI");
+            console.log("leaving...");
+            if (item.classList.contains("liked")) {
+                item.classList.remove("brighter-green");
+            } else {
+                item.classList.remove("brighter-white");
+            }
+        });
+    }
+}
+
+function toggleLiked(item) {
+    if (item.classList.contains("liked")) {
+        item.classList.add("brighter-white");
+        item.classList.remove("liked");
+        item.classList.remove("brighter-green");
+    } else {
+        item.classList.remove("brighter-white");
+        item.classList.add("brighter-green");
+        item.classList.add("liked");
+    }
+}
+
+function enteringResult() {
+    //Sending
+    let arr = [];
+    for (const item of listItems) {
+        arr.push(item.dataset.isliked);
+    }
+    $.post(`${document.URL}/process`, { "arr[]": arr });
+    //href:
+    location.href = "/result";
+}
+
 getFilms();
 getPoster();
+setHoverForFilmItem();
 
-movieListJsp.addEventListener("click", event =>{
-    if(event.target.closest("li")){
+movieListJsp.addEventListener("click", (event) => {
+    if (event.target.closest("li")) {
         const listItem = event.target.closest("li");
-        listItem.classList.toggle("liked");
-        if(listItem.dataset.isliked === "true"){
+        toggleLiked(listItem);
+        if (listItem.dataset.isliked === "true") {
             listItem.dataset.isliked = "false";
         } else {
             listItem.dataset.isliked = "true";
@@ -43,17 +91,4 @@ movieListJsp.addEventListener("click", event =>{
         console.log(listItem.dataset.isliked);
     }
 });
-
-function enteringResult() {
-  //Sending
-  let arr = [];
-  for (const item of listItems) {
-      arr.push(item.dataset.isliked);
-  }
-
-  $.post(`${document.URL}/process`, {"arr[]": arr});
-  //href:
-  location.href="/result";
-}
-
 nextBtn.addEventListener("click", enteringResult);
